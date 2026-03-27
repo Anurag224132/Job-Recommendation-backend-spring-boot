34323892881661d9b29775a51790d0fa3ec29bea
Anurag224132 <1pandeyanurag1@gmail.com>
2026-03-27 15:34:54 +0530

    Initial commit - Job Recommendation Backend (Spring Boot)

---
+++ src/main/java/com/example/job_recommendation_backend/JobRecommendationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.job_recommendation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRecommendationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRecommendationBackendApplication.class, args);
+	}
+
+}
