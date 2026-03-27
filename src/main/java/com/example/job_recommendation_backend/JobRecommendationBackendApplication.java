12f2c7c7a61ef6e2bfd3db0486a4b5611853c963
Anurag224132 <1pandeyanurag1@gmail.com>
2026-03-27 17:17:56 +0530

    fix

--- src/main/java/com/example/job_recommendation_backend/JobRecommendationBackendApplication.java
+++ src/main/java/com/example/job_recommendation_backend/JobRecommendationBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.job_recommendation_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JobRecommendationBackendApplication {
 
 	public static void main(String[] args) {
