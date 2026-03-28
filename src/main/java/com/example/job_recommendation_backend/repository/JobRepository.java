3da0a48e87041aa0f72e72c93b273bbb3efb8907
Anurag224132 <1pandeyanurag1@gmail.com>
2026-03-28 18:51:32 +0530

    added some admin functionality

--- src/main/java/com/example/job_recommendation_backend/repository/JobRepository.java
+++ src/main/java/com/example/job_recommendation_backend/repository/JobRepository.java
@@ -1,6 +1,8 @@
 package com.example.job_recommendation_backend.repository;
 
 import com.example.job_recommendation_backend.entity.Job;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,22 +15,20 @@ import java.util.UUID;
 @Repository
 public interface JobRepository extends JpaRepository<Job, UUID> {
 
-    @Modifying
+    @Modifying(clearAutomatically = true)
     @Transactional
     @Query("UPDATE Job j SET j.user = null WHERE j.user.id = :userId")
     void detachJobsFromUser(UUID userId);
 
-    @Query("SELECT COUNT(j) FROM Job j WHERE j.user.id = :userId")
-    long getCountOfJobGivenRecruiter(@Param("userId") UUID userId);
 
-    org.springframework.data.domain.Page<Job> findByTitleContainingIgnoreCaseOrCompanyNameContainingIgnoreCase(String title, String companyName, org.springframework.data.domain.Pageable pageable);
+    Page<Job> findByTitleContainingIgnoreCaseOrCompanyNameContainingIgnoreCase(String title, String companyName, Pageable pageable);
 
     @Query(value = """
             SELECT 
-                (SELECT COUNT(*) FROM jobs WHERE user_id = :userId AND deleted_at IS NULL) as jobsPosted,
-                (SELECT COUNT(*) FROM applications a JOIN jobs j ON a.job_id = j.id WHERE j.user_id = :userId AND a.deleted_at IS NULL) as totalApplications,
-                (SELECT COUNT(*) FROM applications a JOIN jobs j ON a.job_id = j.id WHERE j.user_id = :userId AND a.status = 'rejected' AND a.deleted_at IS NULL) as rejected,
-                (SELECT COUNT(*) FROM interviews i JOIN jobs j ON i.job_id = j.id WHERE j.user_id = :userId AND i.deleted_at IS NULL) as interviewsScheduled
+                COALESCE((SELECT COUNT(*) FROM jobs WHERE user_id = :userId AND deleted_at IS NULL),0) as jobsPosted,
+                COALESCE((SELECT COUNT(*) FROM applications a JOIN jobs j ON a.job_id = j.id WHERE j.user_id = :userId AND a.deleted_at IS NULL),0) as totalApplications,
+                COALESCE((SELECT COUNT(*) FROM applications a JOIN jobs j ON a.job_id = j.id WHERE j.user_id = :userId AND a.status = 'rejected' AND a.deleted_at IS NULL),0) as rejected,
+                COALESCE((SELECT COUNT(*) FROM interviews i JOIN jobs j ON i.job_id = j.id WHERE j.user_id = :userId AND i.deleted_at IS NULL),0) as interviewsScheduled
             """, nativeQuery = true)
     Object[] getRecruiterAnalytics(@Param("userId") UUID userId);
 }
