2e3d5c580cac42f605daa1534c865c7c494908f2
Anurag224132 <1pandeyanurag1@gmail.com>
2026-03-28 18:34:45 +0530

    added some admin functionality

--- src/main/java/com/example/job_recommendation_backend/repository/InterviewRepository.java
+++ src/main/java/com/example/job_recommendation_backend/repository/InterviewRepository.java
@@ -1,25 +1,17 @@
 package com.example.job_recommendation_backend.repository;
 
 import com.example.job_recommendation_backend.entity.Interview;
-import com.example.job_recommendation_backend.enums.ApplicationStatus;
 import com.example.job_recommendation_backend.enums.InterviewStatus;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface InterviewRepository extends JpaRepository<Interview, UUID> {
 
-    List<Interview> findAllByUserId(UUID userId);
-
-    List<Interview> findAllByJobId(UUID jobId);
-
-    @Query(value = "SELECT * FROM interviews WHERE deleted_at IS NOT NULL", nativeQuery = true)
-    List<Interview> findAllDeletedInterviews();
 
     @Query(" SELECT COUNT(interview) FROM Interview interview WHERE interview.user.id = :userId and interview.status = :status")
     long countInterviewByUserIdAndStatus(@Param("userId") UUID userId, @Param("status") InterviewStatus status);
