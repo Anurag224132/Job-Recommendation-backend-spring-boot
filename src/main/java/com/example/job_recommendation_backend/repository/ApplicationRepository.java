94373142b2f0026781cb3ff86363d4b2456ea333
Anurag224132 <1pandeyanurag1@gmail.com>
2026-03-28 18:29:13 +0530

    added some admin functionality

--- src/main/java/com/example/job_recommendation_backend/repository/ApplicationRepository.java
+++ src/main/java/com/example/job_recommendation_backend/repository/ApplicationRepository.java
@@ -13,22 +13,14 @@ import java.util.UUID;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, UUID> {
-    
-    List<Application> findAllByUserId(UUID userId);
-    
-    List<Application> findAllByJobId(UUID jobId);
 
-    @Query(value = "SELECT * FROM applications WHERE deleted_at IS NOT NULL", nativeQuery = true)
-    List<Application> findAllDeletedApplications();
+    List<Application> findAllByUserId(UUID userId);
+
+    List<Application> findAllByJobId(UUID jobId);
 
     @Query(" SELECT COUNT(ap) FROM Application ap WHERE ap.user.id = :userId ")
     long countApplicationsByUserId(@Param("userId") UUID userId);
 
-    @Query(" SELECT COUNT(ap) FROM Application ap WHERE ap.user.id = :userId and ap.status = :status")
+    @Query("SELECT COUNT(ap) FROM Application ap WHERE ap.user.id = :userId and ap.status = :status")
     long countApplicationsByUserIdAndStatus(@Param("userId") UUID userId, @Param("status") ApplicationStatus status);
-    @Query("SELECT COUNT(a) FROM Application a WHERE a.job.user.id = :userId")
-    long countApplicationsByRecruiterId(@Param("userId") UUID userId);
-
-    @Query("SELECT COUNT(a) FROM Application a WHERE a.job.user.id = :userId AND a.status = :status")
-    long countApplicationsByRecruiterIdAndStatus(@Param("userId") UUID userId, @Param("status") ApplicationStatus status);
 }
