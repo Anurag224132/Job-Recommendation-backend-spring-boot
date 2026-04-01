bb1d81981847c654b3ca2889ff84006c0efac6ae
Anurag224132 <1pandeyanurag1@gmail.com>
2026-04-01 21:46:06 +0530

    fixe

--- src/main/java/com/example/job_recommendation_backend/config/SecurityConfig.java
+++ src/main/java/com/example/job_recommendation_backend/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/auth/**", "/api/jobs/**")
+                                "/api/auth/**")
                         .permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
