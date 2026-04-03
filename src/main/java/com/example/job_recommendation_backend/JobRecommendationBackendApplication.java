4c867a46ce719317937d7147fbae0814102c9570
Anurag224132 <1pandeyanurag1@gmail.com>
2026-04-03 23:35:07 +0530

    fix env variables

--- src/main/java/com/example/job_recommendation_backend/JobRecommendationBackendApplication.java
+++ src/main/java/com/example/job_recommendation_backend/JobRecommendationBackendApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -11,6 +12,17 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class JobRecommendationBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> {
+			if (System.getProperty(entry.getKey()) == null && System.getenv(entry.getKey()) == null) {
+				System.setProperty(entry.getKey(), entry.getValue());
+			}
+		});
+
 		SpringApplication.run(JobRecommendationBackendApplication.class, args);
 	}
 
