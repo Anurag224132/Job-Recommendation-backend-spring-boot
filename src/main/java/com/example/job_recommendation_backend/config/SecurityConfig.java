dae2c28852003af5d49063ecd708ee0c6b0f2c55
Anurag Pandey <12214594anu@gmail.com>
2026-04-16 09:32:01 +0530

    fix

--- src/main/java/com/example/job_recommendation_backend/config/SecurityConfig.java
+++ src/main/java/com/example/job_recommendation_backend/config/SecurityConfig.java
@@ -40,8 +40,7 @@ public class SecurityConfig {
                                 .csrf(csrf -> csrf.disable())
                                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                                 .authorizeHttpRequests(auth -> auth
-                                                .requestMatchers(
-                                                                "/api/auth/**")
+                                                .requestMatchers("/api/auth/**")
                                                 .permitAll()
                                                 .requestMatchers(HttpMethod.GET, "/api/jobs/**").permitAll()
                                                 .requestMatchers(HttpMethod.POST, "/api/applications/calculate-fit",
